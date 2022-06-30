dd16d507785723eb3bd15442badbdf78c4f70004
Gustavo <gar.santeliz@gmail.com>
2022-06-29 23:50:33 -0400

    last touches of the days

--- src/components/Advertisment.jsx
+++ src/components/Advertisment.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import '../style/advertisment.css'
-
+import "../style/advertisment.css";
 
 export const Advertisment = () => {
   return (
-    <div className='advertisment-container'>
-        <img src={require('../images/header-container.jpg')} alt='' />
+    <div className="advertisment-container">
+      <img src={require("../images/advertisment2.jpg")} alt="" />
+      <div className="email-input">
+        <input placeholder="Join us" />
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
 //src\images\advertisment.jpg
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
+import { settings } from "./Setting";
 
-import '../style/gallery.css'
+import "../style/gallery.css";
 
 export const Gallery = () => {
   return (
-    <div className='gallery-container'>Gallery</div>
-  )
-}
+    <div className="gallery-container">
+      <div className="gallery-title">
+        <h1>Gallery</h1>
+      </div>
+      <Slider {...settings}>
+        <div className="slick-img">
+          <img src={require("../images/gallery1.jpg")} alt="" />
+        </div>
+        <div className="slick-img">
+          <img src={require("../images/gallery2.jpg")} alt="" />
+        </div>
+        <div className="slick-img">
+          <img src={require("../images/gallery3.jpg")} alt="" />
+        </div>
+        <div className="slick-img">
+          <img src={require("../images/gallery4.jpg")} alt="" />
+        </div>
+        <div className="slick-img">
+          <img src={require("../images/gallery5.jpg")} alt="" />
+        </div>
+        <div className="slick-img">
+          <img src={require("../images/gallery1.jpg")} alt="" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
