1141210f40a1bacadf5417473a295a69642bce4f
Gustavo <gar.santeliz@gmail.com>
2022-07-02 19:55:06 -0400

    links set up in social media nav

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,27 +1,105 @@
-import {Form, Button} from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { FaFacebookF, FaInstagram, FaTiktok, FaWhatsapp } from "react-icons/fa";
 
+import { NavbarSocialMedia } from "../components/NavbarSocialMedia";
 
-
+import "../style/login.css";
 
 export const Login = () => {
+
+  const handleWhatsappLink = (e) => {
+    e.preventDefault();
+    window.open(
+      "https://wa.me/p/4855065104588916/17865635900",
+      "_blank"
+    );
+  };
+
+  const handleInstagramLink = (e) => {
+    e.preventDefault();
+    window.open("https://www.instagram.com/paradiseminnipancakes/", "_blank");
+  };
+
+  const handleTiktokLink = (e) => {
+    e.preventDefault();
+    window.open("https://www.tiktok.com/@paradiseminnipancakes", "_blank");
+  };
+
+  const handleFacebookLink = (e) => {
+    e.preventDefault();
+    window.open("hhttps://www.facebook.com/paradiseminnipancakes", "_blank");
+  };
+
+
   return (
-    <div>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+    <div className="login-container">
+      <div className="register-logo">
+        <nav className="nav-socialMedia">
+          <div className="icon-socialMedia">
+            <span>
+              <FaFacebookF
+                onClick={handleFacebookLink}
+                className="inner-icon"
+              />
+            </span>
+          </div>
+          <div className="icon-socialMedia">
+            <span>
+              <FaInstagram
+                onClick={handleInstagramLink}
+                className="inner-icon"
+              />
+            </span>
+          </div>
+          <div className="icon-socialMedia">
+            <span>
+              <FaTiktok onClick={handleTiktokLink} className="inner-icon" />
+            </span>
+          </div>
+          <div className="icon-socialMedia">
+            <span>
+              <FaWhatsapp onClick={handleWhatsappLink} className="inner-icon" />
+            </span>
+          </div>
+        </nav>
+      </div>
+      <Form className="login-form">
+        <Form.Group
+          className="mb-3 group-container-login"
+          controlId="formBasicEmail"
+        >
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            id="input-firstName"
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group
+          className="mb-3 group-container-login"
+          controlId="formBasicPassword"
+        >
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            id="input-password"
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Link to='/shoppingCart'>
+        
+        <Button className="btn login" variant="primary" type="submit">
           Login
         </Button>
+        </Link>
+        
+        <Link to="/register">
+          <div className="link-register">
+            If you don't have an account, please click here to register
+          </div>
+        </Link>
       </Form>
     </div>
   );
