3d72057696430613bd7ba1b68fee3cf24700ba44
Gustavo <gar.santeliz@gmail.com>
2022-07-02 21:08:51 -0400

    navbar section moved to a different component

--- src/router/AppMain.jsx
+++ src/router/AppMain.jsx
@@ -3,16 +3,12 @@ import { Footer } from '../components/Footer';
 import { Gallery } from '../components/Gallery';
 import { Header } from '../components/Header';
 import { Ingredients } from '../components/Ingredients';
-import { Navbar } from '../components/Navbar';
 import { NavbarSocialMedia } from '../components/NavbarSocialMedia';
 
 
 export const AppMain = () => {
   return (
     <div className="">
-      <section>
-        <Navbar />
-      </section>
       <section>
         <NavbarSocialMedia />
       </section>
