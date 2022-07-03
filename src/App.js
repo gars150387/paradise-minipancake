b487146bcd99a0d28dbcada5484e79804178e7f3
Gustavo <gar.santeliz@gmail.com>
2022-07-02 21:10:38 -0400

    navbar section added to be rendered in the all pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Login } from "./auth/Login";
 import { Register } from "./auth/Register";
+import { ShoppingCart } from "./cart/ShoppingCart";
+import { Navbar } from "./components/Navbar";
 import { AppMain } from "./router/AppMain";
 
 function App() {
   return (
     <div className="App">
+      <section>
+        <Navbar />
+      </section>
       <Routes>
-      <Route path="/" element={<AppMain />}></Route>
-      <Route exact path="login" element={<Login />}></Route>
-      <Route exact path="register" element={<Register />}></Route>
+        <Route path="/" element={<AppMain />}></Route>
+        <Route exact path="login" element={<Login />}></Route>
+        <Route exact path="register" element={<Register />}></Route>
+        <Route exact path="shoppingCart" element={<ShoppingCart />}></Route>
       </Routes>
     </div>
   );
