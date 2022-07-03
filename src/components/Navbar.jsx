3653f4da8eeac82541f40c9a0db1699181131129
Gustavo <gar.santeliz@gmail.com>
2022-07-02 21:06:32 -0400

    logo placed in center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import '../style/navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className='navbar'>
-        <div className='logo'>
-            <img src={require('../images/logo-no-background.jpeg')} alt='logo' />
+    <nav className="navbar">
+      <Link to='/'>
+        <div className="logo">
+          <img src={require("../images/logo-no-background.jpeg")} alt="logo" />
         </div>
+      </Link>
     </nav>
-  )
-}
+  );
+};
