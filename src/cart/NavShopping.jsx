689a7ddc23294fe8dd9309d49eff6f16f7e3668e
Gustavo <gar.santeliz@gmail.com>
2022-07-04 13:46:53 -0400

    onclick function set to navegate between list of options and checkout

--- src/cart/NavShopping.jsx
+++ src/cart/NavShopping.jsx
@@ -1,26 +1,29 @@
+import "../style/navShoppingCart.css";
 
 
 
+export const NavShopping = ({ show, setShow, cart }) => {
 
-
-import '../style/navShoppingCart.css'
-
-
-export const NavShopping = () => {
   return (
     <>
-        <nav className="nav-shoppingCart">
-            <div className='shoppingCart-title'>
-                <strong><h1>Shopping Cart</h1></strong>
-            </div>
-            <div className="container-info">
-                <div>
-                <i className="bi bi-cart4"></i>
-                </div>
-                <div>
-                    <strong>User</strong></div>
-            </div>
-        </nav>
+      <nav className="nav-shoppingCart">
+        <div className="shoppingCart-title">
+          <strong>
+            <h1 onClick={() => setShow(true)}>{
+              show ? 'Shopping Cart' : "Checkout"
+            }</h1>
+          </strong>
+        </div>
+        <div className="container-info">
+          <div className="icon-cart-container" onClick={() => setShow(false)}>
+            <i className="bi bi-cart4"></i>
+            <strong>{cart.length}</strong>
+          </div>
+          <div>
+            <strong>User</strong>
+          </div>
+        </div>
+      </nav>
     </>
-  )
-}
+  );
+};
