d12f3d2dd1c0989f68a94ab14579d557fbbaae1e
Gustavo <gar.santeliz@gmail.com>
2022-07-06 21:39:32 -0400

    splitting screen to assign 2 space to render added items and checkout info

--- src/cart/Checkout.jsx
+++ src/cart/Checkout.jsx
@@ -2,46 +2,48 @@ import { Counter } from "../helper/Counter";
 import "../style/checkout.css";
 
 export const Checkout = ({ cart, setCart }) => {
+  const handleRemovedItem = (id) => {
+    const removedItem = cart.filter((item) => item.id !== id);
+    setCart(removedItem);
+  };
 
-  const handleRemovedItem = (id) =>{
-    const removedItem = cart.filter((item) => item.id !== id)
-    setCart( removedItem )
-  }
-
-  
-
-
+    
 
   return (
     <div className="checkout-container">
       <div className="added-item-checkout">
-              {cart.map(({ id, title, author, price, img, amount }) => {
-        return (
-          <div className="card-checkout" key={id}>
-            <img src={img} alt='checkout img' />
-            <div className="info-container">
-              <h1>{title}</h1>
-              <p>{author}</p>
-            </div>
-            <strong>
-              <h1>Unit Price: {price}</h1>
-            </strong>
+        {cart.map(({ id, title, author, price, img, amount }) => {
+          return (
+            <div className="card-checkout" key={id}>
+              <img src={img} alt="checkout img" />
+              <div className="info-container">
+                <h1>{title}</h1>
+                <p>{author}</p>
+              </div>
+              <strong>
+                <h1>Unit Price: {price}</h1>
+              </strong>
+              <div className="counter-checkout">
+                <Counter price={ price } amount={amount} />
+              </div>
 
-            <Counter amount={amount} />
-
-            <div className="price-checkout">
-              <h1>Price: {price} </h1>
+              <div className="price-checkout">
+                <h1>Price: {(setIncrease, price)=> {
+                  console.log( setIncrease )
+                  console.log( price )
+                }} </h1>
+              </div>
+              <div className="button-remove-item-checkout">
+                <button onClick={() => handleRemovedItem(id)}>Remove</button>
+              </div>
             </div>
-            <div>
-              <button onClick={() => handleRemovedItem(id)}>Remove</button>
-            </div>
-          </div>
-        );
-      })}
-
+          );
+        })} 
       </div>
-      <div>
+      <div className="checkout-final-description-container">
+        {cart.length === 0 ? <h1>Cart is empty</h1> : <h1>Checkout</h1>}
 
+        {console.log( cart.length )}
       </div>
     </div>
   );
