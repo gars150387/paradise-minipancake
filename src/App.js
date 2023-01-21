1197b61095f936828a4f26e199ef8ff549c19db0
gars150387 <gar.santeliz@gmail.com>
2023-01-21 15:01:44 -0500

    route redefined

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Login } from "./auth/Login";
-import { Register } from "./auth/Register";
+import { Checkout } from "./cart/Checkout";
 import { ShoppingCart } from "./cart/ShoppingCart";
 import { Navbar } from "./components/Navbar";
 import { AppMain } from "./router/AppMain";
@@ -8,14 +7,12 @@ import { AppMain } from "./router/AppMain";
 function App() {
   return (
     <div className="App">
-      <section>
-        <Navbar />
-      </section>
+
+
       <Routes>
         <Route path="/" element={<AppMain />}></Route>
-        <Route exact path="login" element={<Login />}></Route>
-        <Route exact path="register" element={<Register />}></Route>
-        <Route exact path="shoppingCart" element={<ShoppingCart />}></Route>
+        <Route exact path="/shoppingCart" element={<ShoppingCart />}></Route>
+        <Route exact path="/checkout" element={<Checkout />} />
       </Routes>
     </div>
   );
